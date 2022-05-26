990aa9e27476043776dbcab2eb5d1688a745c6cb
KRISHANU1920 <krishanu1920@gmail.com>
2022-05-26 22:14:36 +0530

    Django Admin Page Customized

--- GyaanSangrah/urls.py
+++ GyaanSangrah/urls.py
@@ -18,6 +18,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = "GyaanSangrah Admin"
+admin.site.site_title = "GyaanSangrah Admin Portal"
+admin.site.index_title = "Welcome to GyaanSangrah"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Home.urls')),
